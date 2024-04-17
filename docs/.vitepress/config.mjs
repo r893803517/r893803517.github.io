5323c4552d46eeac92f71062c0c4a83e67e4ecb6
Robin <18665515967@163.com>
2024-04-17 22:26:26 +0800

    delete old index.html

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,72 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from '../utils/side-bar-creator'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Robin's Blog",
+  description: "Blog to record everything",
+
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  
+  srcDir: './packages',
+
+  themeConfig: {
+    logo: './logo.png',
+
+    search: {
+      provider: 'local'
+    },
+
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      // { text: 'Home', link: '/' },
+      // { text: 'Examples', link: '/markdown-examples' }，
+      { text: '首页', link: '/' },
+      {
+        text: '知识整理', items: [
+          { text: '前端', link: '/front-end/css/函数' },
+          { text: '后端', link: '/back-end/nestjs/介绍' },
+          { text: '通用', link: '/common/docker/基本概念' }
+        ]
+      },
+      { text: '面试整理', link: '/markdown-examples' }
+    ],
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' },
+    //       { text: 'JS', link: '/JavaScript/guide' }
+    //     ]
+    //   }
+    // ],
+
+
+    sidebar: {
+      '/front-end/': {
+        text: 'front-end',
+        activeMatch: '/front-end',
+        items: set_sidebar('/packages/front-end', '/front-end')
+      },
+      '/back-end/': {
+        text: 'back-end',
+        items: set_sidebar('/packages/back-end', '/back-end')
+      },
+      '/common/': {
+        text: 'common',
+        items: set_sidebar('/packages/common', '/common')
+      }
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/r893803517' }
+    ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present'
+    }
+  }
+})
